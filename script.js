2aa313d8b4a9c4ea240331623d6b005ce75acf27
Raymond <raymondquicho@gmail.com>
2023-03-26 08:42:49 -0700

    completing search function that gives array of fruits based on user input

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
-	let results = [];
+function search() {
+	// let results = [];
+	const lowerCaseInput = input.value.toLowerCase();
+	const lowerCaseFruitArr = fruit.map(x => x.toLowerCase());
+
+	const filteredSearch = lowerCaseFruitArr.filter(word => word.includes(lowerCaseInput));
+
+	const newFruitArr = filteredSearch.map(x => x.charAt(0).toUpperCase() + x.slice(1));
+
+	return newFruitArr;
+
+	// const filteredSearch = lowerCaseFruitArr.filter(lowerCaseFruitArr.includes(lowerCaseInput) ? results.push(lowerCaseInput) : 'no matches');
+
+	// if(lowerCaseFruitArr.includes(lowerCaseInput)) {
+	// 	results.push(lowerCaseInput);
+	// }
 
 	// TODO
-
-	return results;
+	// if(filteredSearch) {
+	// 	return results.push(filteredSearch);
+	// }
+	
 }
 
 function searchHandler(e) {
